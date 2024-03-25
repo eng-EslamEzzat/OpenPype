0fa58f7f0e618b2e9a71ec45d45f4b6b776b26bd
Seif Ashraf <seifibrahim32@gmail.com>
2024-03-25 16:29:46 +0200

    commit

--- openpype/hosts/equalizer/plugins/publish/extract_lens_distortion_nuke.py
+++ openpype/hosts/equalizer/plugins/publish/extract_lens_distortion_nuke.py
@@ -1,8 +1,7 @@
-from pathlib import Path
-
+import os
 import pyblish.api
 import tde4  # noqa: F401
-
+from mock import patch
 from openpype.lib import import_filepath
 from openpype.pipeline import OptionalPyblishPluginMixin, publish
 
@@ -25,7 +24,7 @@ class ExtractLensDistortionNuke(publish.Extractor,
 
     order = pyblish.api.ExtractorOrder
 
-    def process(self, instance: pyblish.api.Instance):
+    def process(self, instance = pyblish.api.Instance):
 
         if not self.is_active(instance.data):
             return
@@ -33,23 +32,26 @@ class ExtractLensDistortionNuke(publish.Extractor,
         cam = tde4.getCurrentCamera()
         offset = tde4.getCameraFrameOffset(cam)
         staging_dir = self.staging_dir(instance)
-        file_path = Path(staging_dir) / "nuke_ld_export.nk"
+        file_path = os.path.join(staging_dir, "nuke_ld_export.nk")
 
         # import export script from 3DEqualizer
-        exporter_path = instance.data["tde4_path"] / "sys_data" / "py_scripts" / "export_nuke_LD_3DE4_Lens_Distortion_Node.py"  # noqa: E501
-        self.log.debug(f"Importing {exporter_path.as_posix()}")
-        exporter = import_filepath(exporter_path.as_posix())
-        exporter.exportNukeDewarpNode(cam, offset, file_path.as_posix())
+        exporter_path = os.path.join(instance.data["tde4_path"], "sys_data", "py_scripts", "export_nuke_LD_3DE4_Lens_Distortion_Node.py")  # noqa: E501
+        self.log.debug("Importing {}".format(exporter_path))
+
+        # Hide UI with patchin postCustomRequester
+        with patch("tde4.postCustomRequester", lambda *args, **kwargs: 0):
+            exporter = import_filepath(exporter_path)
+            exporter.exportNukeDewarpNode(cam, offset, file_path)
 
         # create representation data
         if "representations" not in instance.data:
             instance.data["representations"] = []
 
         representation = {
-            'name': "lensDistortion",
-            'ext': "nk",
-            'files': file_path.name,
+            'name': "nuke_ld_export",
+            'ext': "ls.nk",
+            'files': os.path.basename(file_path),
             "stagingDir": staging_dir,
         }
-        self.log.debug(f"output: {file_path.as_posix()}")
+        self.log.debug("output: {}".format(file_path))
         instance.data["representations"].append(representation)
